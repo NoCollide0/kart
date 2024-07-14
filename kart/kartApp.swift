921fa6be7d4b40aa2b1ee8c2b64d42b0128493fa
2132112313 <2131>
2024-07-14 18:23:19 +0300

    Initial Commit

---
+++ kart/kartApp.swift
@@ -0,0 +1,17 @@
+//
+//  kartApp.swift
+//  kart
+//
+//  Created by Федор Шашков on 14.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct kartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
